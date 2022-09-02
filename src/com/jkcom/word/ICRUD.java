36e75f125c1ffb32208c5691048b8499a30dbd5b
JinseoKim <eunoia@handong.ac.kr>
2022-09-02 14:53:29 +0900

    2nd commit

--- src/com/jkcom/word/ICRUD.java
+++ src/com/jkcom/word/ICRUD.java
@@ -2,7 +2,9 @@ package com.jkcom.word;
 
 public interface ICRUD {
 	public Object add();
+	public void addWord();
 	public int update(Object obj);
 	public int delete(Object obj);
 	public void selectOne(int id);
+	public void listAll();
 }
