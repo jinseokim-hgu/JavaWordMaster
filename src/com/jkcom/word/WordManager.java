f4fe6aa0b79c8ec1a6b7e814a3bafdfa6acc740e
JinseoKim <eunoia@handong.ac.kr>
2022-09-06 21:44:59 +0900

    Add update menu

--- src/com/jkcom/word/WordManager.java
+++ src/com/jkcom/word/WordManager.java
@@ -37,6 +37,12 @@ public class WordManager {
 			else if (menu == 1) {
 				wordCRUD.listAll();
 			}
+			else if (menu == 5) {
+				wordCRUD.updateItem();
+			}
+			else if (menu == 6) {
+				// delete
+			}
 		}
 		System.out.println("프로그램을 종료합니다.\n");
 	}
